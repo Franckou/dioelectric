d2c0d00ec7cfb0379e34f4a5fd4126a16b129296
Franckou <francoromero015@gmail.com>
2024-07-30 13:44:04 -0300

    correcciones en traduccion

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,19 @@
 const flagsElements = document.getElementById("flags");
 
+const textsToChange = document.querySelectorAll("[data-section");
+
 const changeLanguage = async (language) => {
     const requestJson = await fetch(`./languages/${language}.json`);
     const texts = await requestJson.json();
+    
     console.log(texts);
+
+    for (const textToChange of textsToChange){
+        const section = textToChange.dataset.section;
+        const value = textToChange.dataset.value;
+        
+        textToChange.innerHTML = texts[section][value];
+    }
 };
 
 flagsElements.addEventListener("click", (e) => {
