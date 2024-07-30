a71e57bd0c5243f664cbc0d75f5b3bb5a9c3d1ad
Franckou <francoromero015@gmail.com>
2024-07-30 15:02:12 -0300

    agrega un detector de adlocker y muestra alerta

--- js/scripts.js
+++ js/scripts.js
@@ -20,3 +20,30 @@ flagsElements.addEventListener("click", (e) => {
     const language = e.target.dataset.language || e.target.parentElement.dataset.language;
     changeLanguage(language);
 });
+
+// Función para detectar adblocker
+function detectAdblock() {
+    const adBlockDetected = new Promise((resolve, reject) => {
+        const testAd = document.createElement('div');
+        testAd.innerHTML = '&nbsp;';
+        testAd.className = 'adsbox';
+        document.body.appendChild(testAd);
+        window.setTimeout(() => {
+            if (testAd.offsetHeight === 0) {
+                resolve(true);
+            } else {
+                resolve(false);
+            }
+            testAd.remove();
+        }, 100);
+    });
+
+    return adBlockDetected;
+}
+
+// Usar la función para mostrar notificación
+detectAdblock().then((adBlockDetected) => {
+    if (adBlockDetected) {
+        alert('Parece que estás usando un adblocker. Por favor, considera desactivarlo en nuestro sitio para una mejor experiencia :)');
+    }
+});
